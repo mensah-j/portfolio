e4ec0c129e2d3763b1064b5a4f6bdba69528ca75
Jeffery Opoku-Mensah <mensah@mit.edu>
2024-06-19 04:58:02 -0400

    feat: add list of socials to hero section

--- src/app/(about)/_components/SectionHero/SectionHero.tsx
+++ src/app/(about)/_components/SectionHero/SectionHero.tsx
@@ -1,16 +1,33 @@
 "use client";
 
 import { HeroMessage } from "./HeroMessage";
+import { SocialsList } from "./SocialsList";
 
 export function SectionHero() {
   return (
     <div className="flex flex-col items-center w-full">
-      <div className="flex flex-col items-start gap-3 pt-16 pl-5 w-content">
+      <div className="flex flex-col items-start gap-6 pt-16 pl-5 w-content">
         <HeroMessage
           activities={[
             "build websites.",
             "compose early music.",
             "do geometry.",
+          ]}
+        />
+        <SocialsList
+          socials={[
+            {
+              name: "github",
+              path: "https://github.com/mensah-j",
+            },
+            {
+              name: "stackexchange",
+              path: "https://math.stackexchange.com/users/838822/orange-mushroom",
+            },
+            {
+              name: "youtube",
+              path: "https://youtube.com/@mensah-j",
+            },
           ]}
         />
       </div>
