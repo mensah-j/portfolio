6a656f4633747cfc1c3f54e9d9c7ceafda024305
Jeffery Opoku-Mensah <mensah@mit.edu>
2024-06-19 03:40:41 -0400

    feat: add redirect from about to index

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: "/about",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
