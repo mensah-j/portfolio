46bdbc457dead0a243cd2fe24102243fc4ef803a
Jeffery Opoku-Mensah <mensah@mit.edu>
2024-06-19 12:31:05 -0400

    fix: adjust padding on hero section

--- src/app/(about)/_components/SectionHero/SectionHero.tsx
+++ src/app/(about)/_components/SectionHero/SectionHero.tsx
@@ -6,7 +6,7 @@ import { SocialsList } from "./SocialsList";
 export function SectionHero() {
   return (
     <div className="flex flex-col items-center w-full">
-      <div className="flex flex-col items-start gap-6 pt-16 pl-5 w-content">
+      <div className="flex flex-col items-start gap-6 p-5 pt-16 w-content">
         <HeroMessage
           activities={[
             "build websites.",
