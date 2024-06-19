cbde3d2677a3c0baf945c086c3e8e572e5109946
Jeffery Opoku-Mensah <mensah@mit.edu>
2024-06-19 04:57:14 -0400

    feat: add social icons

---
+++ src/app/(about)/_components/SectionHero/SocialsList/SocialIcon.tsx
@@ -0,0 +1,23 @@
+import "./icon.css";
+
+interface SocialIconProps {
+  name: string;
+  path: string;
+}
+
+export function SocialIcon(props: SocialIconProps) {
+  return (
+    <a
+      href={props.path}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="flex h-9"
+    >
+      <img
+        src={`/socials/${props.name}.svg`}
+        alt={props.name}
+        className="w-auto max-h-full icon"
+      />
+    </a>
+  );
+}
