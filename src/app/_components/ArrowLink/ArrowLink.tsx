96f97515d60bea9ae1aafdb8f4bf04f8e4bb2d0d
Jeffery Opoku-Mensah <mensah@mit.edu>
2024-06-20 09:09:21 -0400

    feat: add dark and bright arrow link

--- src/app/_components/ArrowLink/ArrowLink.tsx
+++ src/app/_components/ArrowLink/ArrowLink.tsx
@@ -1,15 +1,22 @@
 import Link from "next/link";
 import React from "react";
+import mix from "classnames";
 
 export interface ArrowLinkProps {
   href: string;
+  dark?: boolean;
   children: React.ReactNode;
 }
 
 export function ArrowLink(props: ArrowLinkProps) {
   return (
     <Link
-      className="text-gray-600 hover:text-black hover:border-black pb-px font-extrabold transition border-b border-transparent text-md motion-reduce:transition-none"
+      className={mix(
+        !props.dark
+          ? "text-gray-600 hover:text-black hover:border-black"
+          : "text-gray-300 hover:text-white hover:border-white",
+        " pb-px font-extrabold transition border-b border-transparent text-md motion-reduce:transition-none"
+      )}
       href={props.href}
     >
       <span className="inline-flex items-center gap-1 font-bold motion-reduce:transition-none text-md hover:gap-2 transition-all">
