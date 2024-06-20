f1f703a13c6cfb2f49ae414a72a2f6b5f5809e12
Jeffery Opoku-Mensah <mensah@mit.edu>
2024-06-20 09:10:09 -0400

    feat: create footer

---
+++ src/app/(about)/_components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import { Section } from "../Section";
+
+export function Footer() {
+  return (
+    <footer className="w-full">
+      <div className="w-full flex flex-col items-center bg-[#181112]">
+        <div className="flex flex-row items-center justify-end w-content text-white p-2">
+          <p className="text-center text-sm">
+            © 2024 Jeffery Mensah. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
