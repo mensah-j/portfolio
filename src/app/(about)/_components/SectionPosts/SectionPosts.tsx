88bcbb264d1525db405c5f54abc0e736d1ec182c
Jeffery Opoku-Mensah <mensah@mit.edu>
2024-06-24 01:27:54 -0400

    refactor: remove test posts

--- src/app/(about)/_components/SectionPosts/SectionPosts.tsx
+++ src/app/(about)/_components/SectionPosts/SectionPosts.tsx
@@ -1,73 +1,18 @@
 import { ArrowLink } from "@/app/_components/ArrowLink";
-import { Section } from "../Section/Section";
-import { PostList } from "./PostList";
-import { PostItemProps } from "./PostList/PostItem";
-import Latex from "react-latex-next";
-import "katex/dist/katex.min.css";
+import { Section } from "../../../_components/Section/Section";
+import { PostItem } from "@/app/blog/_components/PostList/PostItem";
+import { getPostsPreview } from "@lib/posts";
 
-const posts: PostItemProps[] = [
-  {
-    name: (
-      <span>
-        Finite arithmetic progressions in{" "}
-        <Latex>$\mathbb Z / q \mathbb Z$</Latex>
-      </span>
-    ),
-    path: "/blog",
-    date: "June 20, 2024",
-    length: "5 min",
-    excerpt: (
-      <p>
-        <Latex>
-          Let $q$ be a prime number and $S \subseteq \mathbb Z / q \mathbb Z$ be
-          a subset of size $n$. In this post, we devise an algorithm to
-          determine the number of arithmetic progressions of length $k$ in
-          $\mathbb Z / q \mathbb Z$ disjoint from $S$, with time complexity
-          $\mathscr O (qn \log n)$...
-        </Latex>
-      </p>
-    ),
-  },
-  {
-    name: (
-      <span>
-        Recording on Linux with <code>bluetoothctl</code> and FluidSynth
-      </span>
-    ),
-    path: "/blog",
-    date: "June 20, 2024",
-    length: "5 min",
-    excerpt: `Here's how I record and synthesize MIDI performances on my Kawaii ES110 
-    with real-time MIDI visualization using FluidSynth...
-    `,
-  },
-  {
-    name: "A quick proof of Holditch's Theorem",
-    path: "/blog",
-    date: "June 20, 2024",
-    length: "5 min",
-    excerpt: (
-      <p>
-        <Latex>
-          Let $C$ be a convex curve in the plane, and consider an arbitrary
-          chord of length $\ell$ between two points on $C$. Fix $p$ and $q$ such
-          that $p + q = \ell$, and consider the point that is a distance $p$
-          from one endpoint of the chord and a distance $q$ from the...
-        </Latex>
-      </p>
-    ),
-  },
-];
+const posts = getPostsPreview();
 
 export function SectionPosts() {
   return (
-    <Section
-      name="recent posts"
-      background="#352323"
-      color="#f7f4f0"
-      dark={true}
-    >
-      <PostList posts={posts} />
+    <Section name="recent posts" background="#352323" foreground="#f7f4f0">
+      <div className="flex flex-col gap-12 pt-4 pb-4">
+        {posts.map((post) => (
+          <PostItem key={post.id} {...post} titleColor="#e5d6ba" />
+        ))}
+      </div>
       <ArrowLink dark={true} href="/blog">
         see all posts
       </ArrowLink>
