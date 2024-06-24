5c08c0f60dd6179ff5a7c4719f420bfa2f1c61cb
Jeffery Opoku-Mensah <mensah@mit.edu>
2024-06-24 01:30:48 -0400

    refactor: use image component

--- src/app/(about)/_components/SectionProjects/ProjectItem/TechnologyTag.tsx
+++ src/app/(about)/_components/SectionProjects/ProjectItem/TechnologyTag.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export interface TechnologyTagProps {
   name: string;
   logo: string;
@@ -6,7 +8,14 @@ export interface TechnologyTagProps {
 export function TechnologyTag(props: TechnologyTagProps) {
   return (
     <div className="box-content flex flex-row items-center h-5 shrink-0 gap-1 p-1 bg-white rounded-sm shadow">
-      <img src={props.logo} className="h-full aspect-square" />
+      <div className="h-full aspect-square relative">
+        <Image
+          src={props.logo}
+          alt={props.name}
+          fill
+          style={{ objectFit: "fill" }}
+        />
+      </div>
       <span className="text-xs font-bold text-nowrap">{props.name}</span>
     </div>
   );
