22a461429e399d73ed6b0baadd63c1f474099180
Jeffery Opoku-Mensah <mensah@mit.edu>
2024-06-24 03:00:48 -0400

    chore: css ordering

--- src/app/blog/_components/BlogHeading/BlogHeading.tsx
+++ src/app/blog/_components/BlogHeading/BlogHeading.tsx
@@ -22,13 +22,15 @@ export function BlogHeading() {
   return (
     <Section className="pb-0">
       <h1 className="pt-8 pb-4 text-5xl font-extrabold">/blog</h1>
-      <div className="flex flex-row justify-between items-center w-full">
-        <div className="flex flex-row gap-10 justify-center items-center">
+      <div className="flex flex-row items-center justify-between w-full">
+        <div className="flex flex-row items-center justify-center gap-10">
           {sorts.map((sort) => (
             <SortOption key={sort.name} {...sort} />
           ))}
         </div>
-        <SearchBar />
+        <div className="pb-4">
+          <SearchBar />
+        </div>
       </div>
     </Section>
   );
