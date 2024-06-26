20ece01813444e82700ecfa11e7e88ef227b0035
Jeffery Opoku-Mensah <mensah@mit.edu>
2024-06-26 03:34:43 -0400

    fix: make search bar a div

--- src/app/_components/SearchBar/SearchBar.tsx
+++ src/app/_components/SearchBar/SearchBar.tsx
@@ -1,8 +1,10 @@
+"use client";
+
 import { FaSearch } from "react-icons/fa";
 
 export function SearchBar() {
   return (
-    <button className="flex flex-row items-center justify-between w-40 gap-1 rounded h-7 bg-[#dedede] p-1">
+    <div className="flex flex-row items-center justify-between w-40 gap-1 rounded h-7 bg-[#dedede] p-1">
       <div className="flex flex-row items-center gap-1 font-bold text-[#808080] pl-1">
         <FaSearch size={13} />
         <span>search</span>
@@ -10,6 +12,6 @@ export function SearchBar() {
       <div className="flex flex-row items-center gap-1 h-full pl-2 pr-2 text-[10px] font-bold bg-white rounded-sm shadow">
         <span>ctrl+k</span>
       </div>
-    </button>
+    </div>
   );
 }
