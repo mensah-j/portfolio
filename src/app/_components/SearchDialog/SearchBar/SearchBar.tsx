c600fb9adab65e17e9a6d7ba9c01bf95fa7c0531
Jeffery Opoku-Mensah <mensah@mit.edu>
2024-06-28 17:35:24 -0400

    feat: add search dialog

---
+++ src/app/_components/SearchDialog/SearchBar/SearchBar.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+
+export interface SearchBarProps {
+  onInputChange: (input: string) => void;
+}
+
+export function SearchBar(props: SearchBarProps) {
+  const [input, setInput] = useState("");
+
+  return (
+    <div className="flex flex-row items-center gap-4 bg-gray-100 border border-gray-300 w-full p-4 rounded">
+      <FaSearch size={25} className="text-gray-500" />
+      <input
+        type="text"
+        value={input}
+        placeholder="search"
+        onChange={(e) => {
+          setInput(e.target.value);
+          props.onInputChange(e.target.value);
+        }}
+        className="text-2xl font-bold placeholder:opacity-40 placeholder:text-gray-700 text-gray-700 outline-none w-full bg-inherit"
+      />
+    </div>
+  );
+}
--- src/app/_components/SearchDialog/SearchDialog.tsx
+++ src/app/_components/SearchDialog/SearchDialog.tsx
@@ -1,9 +1,20 @@
+"use client";
+
 import { BaseDialog } from "@/app/_components/BaseDialog";
+import { SearchBar } from "./SearchBar";
 
 export interface SearchDialogProps {
   trigger: React.ReactNode;
 }
 
 export function SearchDialog(props: SearchDialogProps) {
-  return <BaseDialog trigger={props.trigger}>Search</BaseDialog>;
+  return (
+    <BaseDialog trigger={props.trigger}>
+      <SearchBar
+        onInputChange={(input: string) => {
+          console.log(input);
+        }}
+      />
+    </BaseDialog>
+  );
 }
