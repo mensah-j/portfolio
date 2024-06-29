9e04b71b1344daad851d4120236d5f9fc6278122
Jeffery Opoku-Mensah <mensah@mit.edu>
2024-06-29 06:37:20 -0400

    feat: add debounced search prop to dialog

--- src/app/_components/SearchDialog/SearchDialog.tsx
+++ src/app/_components/SearchDialog/SearchDialog.tsx
@@ -2,19 +2,27 @@
 
 import { BaseDialog } from "@/app/_components/BaseDialog";
 import { SearchBar } from "./SearchBar";
+import debounce from "debounce";
 
 export interface SearchDialogProps {
+  search: (query: string) => void;
+
+  children: React.ReactNode;
   trigger: React.ReactNode;
 }
 
 export function SearchDialog(props: SearchDialogProps) {
   return (
     <BaseDialog trigger={props.trigger}>
-      <SearchBar
-        onInputChange={(input: string) => {
-          console.log(input);
-        }}
-      />
+      <div className="p-4">
+        <SearchBar onInputChange={debounce(props.search, 200)} />
+      </div>
+      <div
+        className="overflow-y-auto pl-4 pr-4 pb-4 max-h-[500px]"
+        style={{ scrollbarWidth: "thin" }}
+      >
+        {props.children}
+      </div>
     </BaseDialog>
   );
 }
