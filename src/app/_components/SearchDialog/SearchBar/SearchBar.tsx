06796712345d06ef083fea23d23b39f769152a63
Jeffery Opoku-Mensah <mensah@mit.edu>
2024-07-02 07:32:55 -0400

    feat: make search bar responsive

--- src/app/_components/SearchDialog/SearchBar/SearchBar.tsx
+++ src/app/_components/SearchDialog/SearchBar/SearchBar.tsx
@@ -2,6 +2,9 @@
 
 import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import { FaXmark } from "react-icons/fa6";
+import styles from "./SearchBar.module.css";
+import mix from "classnames";
 
 export interface SearchBarProps {
   onInputChange: (input: string) => void;
@@ -11,8 +14,8 @@ export function SearchBar(props: SearchBarProps) {
   const [input, setInput] = useState("");
 
   return (
-    <div className="flex flex-row items-center gap-4 bg-white border border-gray-300 w-full p-4 rounded">
-      <FaSearch size={25} className="text-gray-500" />
+    <div className="flex flex-row items-center gap-3 sm:gap-4 bg-white border border-gray-300 w-full pt-2 pb-2 pl-3 pr-3 sm:p-4 rounded">
+      <FaSearch className="text-gray-500 shrink-0 text-2xl hidden sm:block" />
       <input
         type="text"
         value={input}
@@ -21,8 +24,19 @@ export function SearchBar(props: SearchBarProps) {
           setInput(e.target.value);
           props.onInputChange(e.target.value);
         }}
-        className="text-2xl font-bold placeholder:opacity-40 placeholder:text-gray-700 text-gray-700 outline-none w-full bg-inherit"
+        className="text-xl sm:text-2xl font-bold placeholder:opacity-40 placeholder:text-gray-700 text-gray-700 outline-none w-full bg-inherit"
       />
+      {input && (
+        <button
+          className={mix("group", styles["clear-input"])}
+          onClick={() => {
+            setInput("");
+            props.onInputChange("");
+          }}
+        >
+          <FaXmark className="text-gray-500 group-hover:text-gray-700 text-xl sm:text-2xl" />
+        </button>
+      )}
     </div>
   );
 }
