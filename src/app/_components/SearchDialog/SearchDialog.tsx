4adcbefb39d55fec18b3cc883b2b5a9671f6df81
Jeffery Opoku-Mensah <mensah@mit.edu>
2024-07-02 07:34:08 -0400

    feat: add hotkey listeners to search dialog

--- src/app/_components/SearchDialog/SearchDialog.tsx
+++ src/app/_components/SearchDialog/SearchDialog.tsx
@@ -1,28 +1,67 @@
 "use client";
-
-import { BaseDialog } from "@/app/_components/BaseDialog";
+import { SearchTrigger } from "./SearchTrigger";
+import { BaseDialog } from "../BaseDialog";
 import { SearchBar } from "./SearchBar";
 import debounce from "debounce";
+import { useEffect, useState } from "react";
+import { FaArrowLeft } from "react-icons/fa";
 
 export interface SearchDialogProps {
   search: (query: string) => void;
 
   children: React.ReactNode;
-  trigger: React.ReactNode;
 }
 
 export function SearchDialog(props: SearchDialogProps) {
+  const [open, setOpen] = useState(false);
+  const ctrlKListener = (e: KeyboardEvent) => {
+    if (e.key === "k" && e.ctrlKey) {
+      e.preventDefault();
+      setOpen(!open);
+    }
+  };
+
+  const escListener = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      e.preventDefault();
+      setOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", ctrlKListener);
+    document.addEventListener("keydown", escListener);
+    return () => {
+      document.removeEventListener("keydown", ctrlKListener);
+      document.removeEventListener("keydown", escListener);
+    };
+  });
+
   return (
-    <BaseDialog trigger={props.trigger}>
-      <div className="p-4">
-        <SearchBar onInputChange={debounce(props.search, 200)} />
-      </div>
-      <div
-        className="overflow-y-auto pl-4 pr-4 pb-4 max-h-[500px]"
-        style={{ scrollbarWidth: "thin" }}
+    <>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          setOpen(!open);
+        }}
       >
-        {props.children}
-      </div>
-    </BaseDialog>
+        <SearchTrigger />
+      </button>
+      <BaseDialog open={open} onClose={() => setOpen(false)}>
+        <div className="flex flex-col h-full">
+          <div className="flex flex-row items-center gap-4 p-4">
+            <div className="sm:hidden">
+              <FaArrowLeft
+                className="text-xl text-gray-500 "
+                onClick={() => setOpen(false)}
+              />
+            </div>
+            <SearchBar onInputChange={debounce(props.search, 200)} />
+          </div>
+          {props.children}
+        </div>
+      </BaseDialog>
+    </>
   );
 }
