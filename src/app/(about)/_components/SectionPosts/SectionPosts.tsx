5875fdb1d62ae712ea93ea17aba556f19c585ec1
Jeffery Opoku-Mensah <mensah@mit.edu>
2024-07-02 08:10:25 -0400

    fix: remove old post item component

--- src/app/(about)/_components/SectionPosts/SectionPosts.tsx
+++ src/app/(about)/_components/SectionPosts/SectionPosts.tsx
@@ -1,6 +1,6 @@
 import { ArrowLink } from "@/app/_components/ArrowLink";
 import { Section } from "../../../_components/Section/Section";
-import { PostItem } from "@/app/blog/_components/PostList/PostItem";
+import { PostItem } from "@/app/blog/_components/PostItem";
 import posts from "@lib/posts";
 
 export function SectionPosts() {
