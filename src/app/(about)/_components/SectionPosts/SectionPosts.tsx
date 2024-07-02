ed27c05a26417e024ba02b9a9d0e0b79f02b8f54
Jeffery Opoku-Mensah <mensah@mit.edu>
2024-07-02 07:30:05 -0400

    fix: preview only three posts on home page

--- src/app/(about)/_components/SectionPosts/SectionPosts.tsx
+++ src/app/(about)/_components/SectionPosts/SectionPosts.tsx
@@ -7,7 +7,7 @@ export function SectionPosts() {
   return (
     <Section name="recent posts" background="#352323" foreground="#f7f4f0">
       <div className="flex flex-col gap-12 pt-4 pb-4">
-        {posts.preview.map((post) => (
+        {posts.preview.slice(0, 3).map((post) => (
           <PostItem key={post.id} {...post} titleColor="#e5d6ba" />
         ))}
       </div>
