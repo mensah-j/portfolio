8116fc3842a62121ea2cd7dfcc0320c34fa5a2ef
Jeffery Opoku-Mensah <mensah@mit.edu>
2024-07-02 07:30:51 -0400

    feat: add on close handler to dialog

--- src/app/_components/BaseDialog/BaseDialog.tsx
+++ src/app/_components/BaseDialog/BaseDialog.tsx
@@ -4,26 +4,27 @@ import styles from "./BaseDialog.module.css";
 import mix from "classnames";
 
 export interface BaseDialogProps {
-  trigger: React.ReactNode;
+  open?: boolean;
+  onClose?: () => void;
   children: React.ReactNode;
 }
 
 export function BaseDialog(props: BaseDialogProps) {
   return (
-    <Dialog.Root>
-      <Dialog.Trigger>{props.trigger}</Dialog.Trigger>
+    <Dialog.Root open={props.open}>
       <Dialog.Portal>
         <VisuallyHidden.Root>
           <Dialog.Title>Search</Dialog.Title>
         </VisuallyHidden.Root>
         <Dialog.Overlay
           className={mix(styles["overlay"], "fixed inset-0 bg-black/50")}
+          onClick={() => props.onClose && props.onClose()}
         />
         <Dialog.Content
           aria-describedby={undefined}
           className={mix(
             styles["content"],
-            "fixed top-0 left-1/2 -translate-x-1/2 translate-y-[25%] w-[700px] h-fit rounded bg-gray-100",
+            "overflow-hidden fixed shadow-lg shadow-[#575757] top-0 left-0 sm:left-1/2 sm:-translate-x-1/2 sm:translate-y-[100px] w-full h-full sm:w-[600px] md:w-[700px] sm:h-fit sm:rounded bg-gray-100",
           )}
         >
           {props.children}
