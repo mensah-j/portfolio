69f0cc644315646bf06a353f70d7fd5483ae053c
Jeffery Opoku-Mensah <mensah@mit.edu>
2024-11-17 12:57:37 -0500

    feat: try tikzjax

--- src/app/math/page.tsx
+++ src/app/math/page.tsx
@@ -1,13 +1,21 @@
 import { UnderConstruction } from "../_components/UnderConstruction";
+import tex2svg from "node-tikzjax";
 
 export const metadata = {
   title: "Jeffery Mensah | Math",
 };
 
 export default function Page() {
+  async function compileTikz() {
+    "use server";
+    return await tex2svg(
+      "\\begin{tikzpicture} \\node (a) at (0,0) {}; \\end{tikzpicture}",
+    );
+  }
   return (
     <main className="flex flex-col items-center grow">
       <UnderConstruction />
+      <div>{compileTikz()}</div>
     </main>
   );
 }
