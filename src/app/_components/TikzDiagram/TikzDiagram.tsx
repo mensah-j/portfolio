4531445048417aa0f408491e37bc10b20dacda1f
Jeffery Opoku-Mensah <mensah@mit.edu>
2024-11-17 22:55:52 -0500

    fix: await for tikz svg source

--- src/app/_components/TikzDiagram/TikzDiagram.tsx
+++ src/app/_components/TikzDiagram/TikzDiagram.tsx
@@ -4,12 +4,12 @@ interface TikzDiagramProps {
   source: string;
 }
 
-export function TikzDiagram(props: TikzDiagramProps) {
+export async function TikzDiagram(props: TikzDiagramProps) {
   return (
     <main className="flex flex-col items-center grow p-2">
       <div
         style={{ zoom: 1.5 }}
-        dangerouslySetInnerHTML={{ __html: readTikz(props.source) }}
+        dangerouslySetInnerHTML={{ __html: await readTikz(props.source) }}
       ></div>
     </main>
   );
