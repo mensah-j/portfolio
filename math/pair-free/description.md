0ee927961563a898443ffb386547efdfe1e65672
Jeffery Mensah <mensah@mit.edu>
2026-04-19 17:54:57 -0400

    miscellaneous notes

--- math/pair-free/description.md
+++ math/pair-free/description.md
@@ -5,4 +5,4 @@ date: 2026-04-20
 finished: false
 ---
 
-We investigate domino tilings in two and three dimensions and determine geometric conditions which force tilings to contain specified substructures. In particular, for a special class of regions in the two-dimensional lattice, we derive a bound on the number of flippable pairs and \say{windmill-shaped} substructures a tiling must contain. Finally, we extend the result to three dimensions.
+We investigate domino tilings in two dimensions and determine geometric and topological conditions which force tilings to contain specified substructures. In particular, for a special class of regions in the two-dimensional lattice, we derive a bound on the number of flippable pairs and "windmill-shaped" substructures a tiling must contain. Finally, we extend the result to three dimensions.
--- src/app/math/_components/MathNotes/MathNotes.tsx
+++ src/app/math/_components/MathNotes/MathNotes.tsx
@@ -4,14 +4,29 @@ import { MathNote } from "../MathNote/MathNote";
 
 export function MathItems() {
   return (
-    <Section name="drafts" background="#f4f4f4">
-      <ul className="list-disc list-outside pl-10 space-y-3">
-        {math.all.map((note) => (
-          <li key={note.id} className="pl-4">
-            <MathNote math={note} />
-          </li>
-        ))}
-      </ul>
-    </Section>
+    <>
+      <Section name="drafts" background="#f4f4f4">
+        <ul className="list-disc list-outside pl-10 space-y-3">
+          {math.all
+            .filter((note) => note.type == "draft")
+            .map((note) => (
+              <li key={note.id} className="pl-4">
+                <MathNote math={note} />
+              </li>
+            ))}
+        </ul>
+      </Section>
+      <Section name="miscellaneous" background="#f4f4f4">
+        <ul className="list-disc list-outside pl-10 space-y-3">
+          {math.all
+            .filter((note) => note.type == "miscellaneous")
+            .map((note) => (
+              <li key={note.id} className="pl-4">
+                <MathNote math={note} />
+              </li>
+            ))}
+        </ul>
+      </Section>
+    </>
   );
 }
