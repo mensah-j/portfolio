e88c9a5be671d9650c9432acf26d102163b39504
Jeffery Mensah <mensah@mit.edu>
2026-04-19 01:28:40 -0400

    feat: new draft

--- src/app/math/_components/MathNotes/MathNotes.tsx
+++ src/app/math/_components/MathNotes/MathNotes.tsx
@@ -5,7 +5,7 @@ import { MathNote } from "../MathNote/MathNote";
 export function MathItems() {
   return (
     <Section name="drafts" background="#f4f4f4">
-      <ul className="list-disc list-outside pl-10 space-y-1">
+      <ul className="list-disc list-outside pl-10 space-y-3">
         {math.all.map((note) => (
           <li key={note.id} className="pl-4">
             <MathNote math={note} />
